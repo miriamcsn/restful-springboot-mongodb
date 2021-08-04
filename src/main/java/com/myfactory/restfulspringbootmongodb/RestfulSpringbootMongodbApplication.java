ada2c4f48d6dc6e75a092c5f989183090edfe6f2
miriamcsn <mceli87@gmail.com>
2021-08-04 18:42:39 -0300

    Upload first files

---
+++ src/main/java/com/myfactory/restfulspringbootmongodb/RestfulSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.myfactory.restfulspringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myfactory/restfulspringbootmongodb/RestfulSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfactory.restfulspringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
