2eb3bf8857c2b33b7aca83700033d225d09c8cfb
miriamcsn <mceli87@gmail.com>
2021-08-05 10:01:31 -0300

    Using DTO pattern to return users

--- src/main/java/com/myfactory/restfulspringbootmongodb/resources/UserResource.java
+++ src/main/java/com/myfactory/restfulspringbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.myfactory.restfulspringbootmongodb.resources;
 
 import com.myfactory.restfulspringbootmongodb.domain.User;
+import com.myfactory.restfulspringbootmongodb.dto.UserDTO;
 import com.myfactory.restfulspringbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,9 +22,10 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> userDTOList = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOList);
 
 //        User maria = new User("1", "Maria Brown", "maria@gmail.com");
 //        User alex = new User("2", "Alex Green", "alex@gmail.com");
